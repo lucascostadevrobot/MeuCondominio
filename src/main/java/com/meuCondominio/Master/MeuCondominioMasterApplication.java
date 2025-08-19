3a12d1ed00adb358f86587619d4fca32370ac9c8
Lucas da Costa <49735129+lucascostadevrobot@users.noreply.github.com>
2025-08-19 20:41:32 -0300

    TAREFA01- Cria principais estruturas dos packages v1

---
+++ src/main/java/com/meuCondominio/Master/MeuCondominioMasterApplication.java
@@ -0,0 +1,13 @@
+package com.meuCondominio.Master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuCondominioMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuCondominioMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meuCondominio/Master/MeuCondominioMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meuCondominio.Master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuCondominioMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
