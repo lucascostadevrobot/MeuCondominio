8471a7952825f0450b036f01a0c0358da013efce
Lucas da Costa <49735129+lucascostadevrobot@users.noreply.github.com>
2025-09-04 20:24:39 -0300

    TAREFA04- Cria UsuarioRepository com metodo para buscar usuariovia JPQL v1

---
+++ src/main/java/com/meuCondominio/Master/Repository/Usuarios/UsuarioRepository.java
@@ -0,0 +1,20 @@
+package com.meuCondominio.Master.Repository.Usuarios;
+
+import com.meuCondominio.Master.Models.Usuarios.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UsuarioRepository extends JpaRepository<Integer, Usuario> {
+
+    /**
+     * findByUsername:
+     * Montamos uma JPQL para buscarmos um Usuário pelo user_name.
+     * existByUsername:
+     * Verificar se existie o usuário em questão.
+     * */
+    @Query("SELECT * FROM Usuario e JOIN FETCH e.roles WHERE e.user_name = (:user_name)")
+    public Usuario findByUsername(@Param("user_name") String username);
+
+    boolean existByUsername(String username);
+}
